6ae2dab4adf4f75557f50cfc31ff1e37fcb7d097
abdo09adham@gmail.com <abdo09adham@gmail.com>
2025-02-04 23:03:35 +0200

    Register Name Changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthContext } from "./context/AuthContext";
 import React from "react";
